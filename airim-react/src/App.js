48d3d9536ed1d15b7051b4be3b55a83bc3227936
syugi90 <altnr23@naver.com>
2020-10-12 00:11:40 +0900

    add source

--- airim-react/src/App.js
+++ airim-react/src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import Home from "./components/Home";
-import CourseIntro from "./components/CourseIntro";
-import CreateTalk from "./components/CreateTalk";
-import SassComponent from "./SassComponent";
-import "./App.css";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import CourseIntro from './components/CourseIntro';
+import CreateTalk from './components/CreateTalk';
+import HistorySample from './components/HistorySample';
+
+import './App.css';
 
 const App = () => {
   return (
     <div>
-      <SassComponent />
-      <Home />
-      <CourseIntro />
-      <CreateTalk />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/intro" component={CourseIntro} />
+        <Route path="/talk" component={CreateTalk} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
--- airim-react/src/components/Home.js
+++ airim-react/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const style = {
@@ -6,22 +7,21 @@ const Home = () => {
     // color: 'aqua',
     // fontSize: 24, // 기본 단위 px
     // padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
-  
+  };
+
   return (
     <div className="Home">
-      <nav>
+      <nav>
         <div>경연</div>
       </nav>
-    
       <ul>
-        <li>#파이썬</li>
-        <li>#자연어처리</li>
-        <li>#R머신러닝</li>
+        <li>#파이썬</li>
+        <li>#자연어처리</li>
+        <li>#R머신러닝</li>
       </ul>
-      
-      <button style={style}>내강의 만들기</button>
-    </div>
+      <button style={style}>내강의 만들기</button>   
+      <Link to="/CourseIntro">강의소개</Link>
+    </div>
   );
 };
 
