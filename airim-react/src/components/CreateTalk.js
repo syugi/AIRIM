29391a50e93cb8b86ce80d96d8f887440fb86a69
syugi90 <altnr23@naver.com>
2020-10-18 23:00:31 +0900

    add useReducer

--- airim-react/src/components/CreateTalk.js
+++ airim-react/src/components/CreateTalk.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useInputs from './common/useInputs';
+import Layout from './common/Layout';
 
-const CreateTalk = ({ type, content, onChange, onCreate }) => {
-  //function CreateTalk({ type, content, onChange, onCreate }) {
+const CreateTalk = ({ history }) => {
+  //렌더링 될때마다 호출됨
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname,
+    });
+  });
+
+  const [state, onChange] = useInputs({
+    name: '',
+    nickname: '',
+  });
+
+  const { name, nickname } = state;
+
   return (
-    <div>
-      <input name="type" placeholder="타입" onChange={onChange} value={type} />
-      <input
-        name="content"
-        placeholder="내용"
-        onChange={onChange}
-        value={content}
-      />
-      <button onClick={onCreate}>등록</button>
-    </div>
+    <Layout history={history}>
+      <div>
+        <div>
+          <input
+            name="name"
+            placeholder="이름"
+            onChange={onChange}
+            value={name}
+          />
+          <input
+            name="nickname"
+            placeholder="닉네임"
+            onChange={onChange}
+            value={nickname}
+          />
+        </div>
+        <div>
+          <div>
+            <b>이름: </b>
+            {name}
+          </div>
+          <div>
+            <b>닉네임: </b>
+            {nickname}
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
