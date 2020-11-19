bf92eb4622dfde8faf79006138abc2ba4104f610
syugi90 <altnr23@naver.com>
2020-11-19 17:45:07 +0900

    add koa router test

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -1,10 +1,34 @@
+require("dotenv").config();
+
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
-app.use((ctx) => {
-  ctx.body = "hello world";
-});
+const router = new Router();
+const api = require("./api");
 
-app.listen(4000, () => {
-  console.log("Listening to port 4000");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+// mongodb 연결
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    //useMongoClient: true,
+  })
+  .then((response) => {
+    console.log("Successfully connected to mongodb");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const port = process.env.PORT || 4000;
+
+router.use("/api", api.routes()); // api 라우트를 /api 경로 하위 라우트
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log("heurm server is listening to port " + port);
 });
